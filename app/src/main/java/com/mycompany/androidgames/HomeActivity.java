d3e0dd1a73d478d151d7e407e9d491b19bbc1c31
Amandeep Singh <amandees@thoughtworks.com>
2015-06-09 17:03:22 +0530

    added navigation drawer..got some problems...so adding it here

--- app/src/main/java/com/mycompany/androidgames/HomeActivity.java
+++ app/src/main/java/com/mycompany/androidgames/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.mycompany.androidgames;
 
+import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -28,9 +32,34 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         ListView listView = (ListView) findViewById(R.id.games_list_view);
         addAndShowGames(listView);
+        addItemsToNavigationDrawer();
+        ((LinearLayout)this.findViewById(R.id.home_activity)).setVisibility(LinearLayout.GONE);
 
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.navigation_drawer_button, R.string.hello_world, R.string.app_name);
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((DrawerLayout)this.findViewById(R.id.drawer_layout)).setVisibility(DrawerLayout.VISIBLE);
+//        getSupportActionBar().hi
+//        getSupportActionBar().hi
+
+
+    }
+
+    public void addItemsToNavigationDrawer() {
+        getSupportActionBar();
+        ListView listView = (ListView) findViewById(R.id.left_drawer);
+        ArrayList<String> drawerListNames = new ArrayList<>();
+        System.out.println("hiiiiiiiiiiiiii");
+        drawerListNames.add("Play Sudoku");
+        drawerListNames.add("play TicTacToe");
+//        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view_adapter, R.id.list_view_adapter, drawerListNames);
+        listView.setAdapter(arrayAdapter);
     }
 
     public void addAndShowGames(ListView listView) {
@@ -54,6 +83,7 @@ public class HomeActivity extends ActionBarActivity {
 //                    fragmentTransaction.add(R.id.home_activity, fragment);
 //                    fragmentTransaction.commit();
                     setContentView(R.layout.fragment_add_player_names);
+//                    getSupportActionBar().hide(); to hide action bar....
 
 
                 }
@@ -81,7 +111,16 @@ public class HomeActivity extends ActionBarActivity {
 //        System.out.println("************************"+player1_name+"************"+player2_name);
     }
 
-//    public void startGame(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        System.out.println(menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+
+    //    public void startGame(View view) {
 //
 //    }
 
@@ -94,17 +133,20 @@ public class HomeActivity extends ActionBarActivity {
 //    }
 
 //    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.navigation_drawer_button_id) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
